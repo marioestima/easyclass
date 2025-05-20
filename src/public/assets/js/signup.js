abb672d7fd15f1a769e80e3f451b3806a55eadde
mario estima <marioestimahackz@gmail.com>
2025-05-20 12:45:09 +0100

    Refactor:Add formData in SignUp

--- src/public/assets/js/signup.js
+++ src/public/assets/js/signup.js
@@ -1,41 +1,25 @@
 const signUpForm = document.getElementById("sign-up");
 const informedUserType = localStorage.getItem("usertype");
 
-async function verifyUserTypeAndSendAdditionalInformation(userinformation) {
-    try {
-        const user = await userinformation;
-
-        if (user === "student")
-            return user
-        else if (user === "teacher")
-            // pegar o nome do documento informando e o nome da escola
-            alert("user type is teacher");
-        else if (user === "admin")
-            // pegar o nome do documento informando e codigo da escola
-            alert("user type is admin");
-        return null;
-    } catch(err) {
-        console.log("invalid informations!", err)
-    }
-}
 
 signUpForm.addEventListener("submit", async function (event) {
     event.preventDefault();
     try {
 
+        const formData = new formData(signUpForm);
         const response = await fetch("http://localhost:8000/signup", {
             method: "POST",
             headers: {
                 "Content-Type": "Application/json",
             },
-            body: JSON.stringify({ firstName, lastName, informedUserType })
+            body: formData
         });
         const data = await response.json();
         console.log("server response: ", data);
 
         if (response.ok)
             window.location.href = "/login";
-        alert(data.message, "intern server error");
+        alert(data.message, "internal server  :(");
 
     } catch (err) {
         console.error("Error in request");
